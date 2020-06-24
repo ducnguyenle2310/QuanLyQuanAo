de5910e8a01fc7af0aa460b8583dc618cf1f1edc
nt9747 <36836563+nt9747@users.noreply.github.com>
2020-06-25 00:06:36 +0700

    Update Gio Hang

--- Downloads/TTnhom/TTnhom/Quản lý cửa hàng bán quần áo/Myweb/MyWeb/Models/GioHang.cs
+++ Downloads/TTnhom/TTnhom/Quản lý cửa hàng bán quần áo/Myweb/MyWeb/Models/GioHang.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWeb.Models
+{
+    public class GioHang
+    {
+
+        QuanLiQuanAoEntities1 db = new QuanLiQuanAoEntities1();
+        public int iMaQuanAo { get; set; }
+        public string sTenQuanAo { get; set; }
+        public string sAnh { get; set; }
+        public double dDonGia { get; set; }
+        public int iSoLuong { get; set; }
+        public double ThanhTien
+        {
+            get { return iSoLuong * dDonGia; }
+        }
+
+        //Ham tao cho gio hang
+        public GioHang(int MaQuanAo)
+        {
+            iMaQuanAo = MaQuanAo;
+            QuanAo quanao = db.QuanAos.Single(n => n.MaQuanAo == iMaQuanAo);
+            sTenQuanAo = quanao.TenQuanAo;
+            sAnh = quanao.Anh;
+            dDonGia = double.Parse(quanao.GiaBan.ToString());
+            iSoLuong = 1;
+        }
+    }
+}
